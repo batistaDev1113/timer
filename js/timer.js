a0f89cbb2ff36533428b015b66a3ab7a5433d7bb
yundeveloper <yuniorbatista1113@gmail.com>
2019-04-28 14:57:42 -0400

    new project added

---
+++ js/timer.js
@@ -0,0 +1,39 @@
+
+// grab necessary elements
+const hour = document.querySelector('div#timeHour h2');
+const min = document.querySelector('div#timeMin h2');
+const sec = document.querySelector('div#timeSec h2');
+let month = document.querySelector('div#month h2');
+const day = document.querySelector('div#day h2');
+const year = document.querySelector('div#year h2');
+const seconds = document.querySelector('h2.effect');
+let meridiem = document.querySelector('div#am-pm h2');
+
+
+//array of months
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// function to update Date object
+function updateClock() {
+  const d = new Date();
+  updateMeridiem(d.getHours());
+  sec.innerHTML = d.getSeconds();
+  min.innerHTML = d.getMinutes();
+  let monthTrack = months[d.getMonth()];
+  month.innerHTML = monthTrack;
+  day.innerHTML = d.getDate();
+  year.innerHTML = d.getFullYear();
+  seconds.classList.toggle('effect');
+  setTimeout(updateClock, 1000);
+}
+
+updateClock();
+
+function updateMeridiem(currentHour) {
+  hour.innerHTML = currentHour % 12;
+  meridiem.innerHTML = 'PM';
+  if (currentHour % 12 == 0) {
+    hour.innerHTML = (currentHour % 12) + 1;
+    meridiem.innerHTML = 'AM';
+  }
+}
